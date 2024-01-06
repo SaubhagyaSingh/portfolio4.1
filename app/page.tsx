322c60aaed88f94cfef9c5ddcca2fde1aba8bd6a
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2024-01-07 01:58:39 +0530

    base page completed

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
         className="flex items-center w-full h-full bg-cover bg-center"
         style={{ backgroundImage: "url(/main-bg.webp)" }}
       >
-        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+        <div className="  pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-8 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
             Make Anything Possible with
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              Web Development
+             {" "} Web Development
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
@@ -22,29 +22,80 @@ export default function Home() {
           <div className="flex-col md:flex-row hidden md:flex">
             <Link
               href="/my-skills"
-              className=" group rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className="px-3 py-3 mr-4 group relative rounded-[20px] bg-blue-500 hover:bg-blue-400  text-lg text-white max-w-[200px]"
             >
               Learn More
             </Link>
 
             <Link
               href="/my-projects"
-              className=" group rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className=" mr-4 group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               My Projects
             </Link>
 
             <Link
-              href="/my-contact"
-              className=" group rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              href="/contact-me"
+              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
+              <div className="absolute rounded-[20px] z-[10] bg-white inset-0 opacity-0 group relative-hover:opacity-20" />
               Contact me
             </Link>
           </div>
         </div>
       </div>
+
+
+      <div className=" flex absolute bottom-10 z-[20] right-5 flex-col md:hidden ">
+            <Link
+              href="/my-skills"
+              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              Learn More
+            </Link>
+
+            <Link
+              href="/my-projects"
+              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              My Projects
+            </Link>
+
+            <Link
+              href="/contact-me"
+              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              
+              Contact me
+            </Link>
+          </div>
+          <div className=" absolute bottom-0 right-0 z-[10]">
+            <Image
+            src="/horse.png"
+            alt="horse"
+            height={300}
+            width={300}
+            className="absolute right-55 top-40"
+             />
+             <Image
+             src="/cliff.webp"
+             alt="cliff"
+             width={480}
+             height={480}
+             />
+          </div>
+          <div className=" absolute bottom-0 z-[5] w-full h-full">
+          <Image src="/trees.webp" alt="trees" width={2000} height={2000}
+          className="w-full h-full"/>
+            
+            <Image
+            src="/stars.png"
+            alt="horse"
+            height={300}
+            width={300}
+            className="absolute left-0 top-10 z-[10]"
+             />
+          </div>
     </main>
   );
 }

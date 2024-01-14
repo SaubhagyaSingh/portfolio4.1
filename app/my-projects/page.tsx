584dbc6838af00c15dceff76074a1fe64d6bc64e
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2024-01-15 05:12:06 +0530

    UPDATED

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -7,7 +7,17 @@ const Page = () => {
     <div 
     style={{backgroundImage:"url(/mountains.jpg)"}}
     className="w-screen h-screen flex items-center justify-center bg-center bg-cover">
-    
+      <h1 className='font-semibold text-white text-[40px] mr-16'>
+      <br></br>W
+      <br></br> E
+      <br></br>B
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+             {" "} 
+            <br></br> D<br></br>
+             E<br></br>
+             V{" "} 
+            </span>
+            </h1>
     <div
     className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
       {WebProjects.map((project,index)=>(

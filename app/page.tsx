6a4f271fd5dbd510885c214d135245d9497cd63b
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2024-01-14 23:11:58 +0530

    3 pages done

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="w-screen h-screen relative">
-      <div
-        className="flex items-center w-full h-full bg-cover bg-center"
+      
+      <div className="flex items-center w-full h-full bg-cover bg-center"
         style={{ backgroundImage: "url(/main-bg.webp)" }}
       >
         <div className="  pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-8 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
             Make Anything Possible with
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
              {" "} Web Development
             </span>
           </h1>
@@ -29,14 +29,14 @@ export default function Home() {
 
             <Link
               href="/my-projects"
-              className=" mr-4 group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className=" mr-4 group relative rounded-[20px] border border-white bg-transparent hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
               My Projects
             </Link>
 
             <Link
               href="/contact-me"
-              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className=" group relative rounded-[20px] border border-white bg-transparent hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
               <div className="absolute rounded-[20px] z-[10] bg-white inset-0 opacity-0 group relative-hover:opacity-20" />
               Contact me
@@ -45,8 +45,7 @@ export default function Home() {
         </div>
       </div>
 
-
-      <div className=" flex absolute bottom-10 z-[20] right-5 flex-col md:hidden ">
+          <div className=" flex absolute bottom-10 z-[20] right-5 flex-col md:hidden ">
             <Link
               href="/my-skills"
               className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
@@ -56,19 +55,19 @@ export default function Home() {
 
             <Link
               href="/my-projects"
-              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className=" group relative rounded-[20px] border border-white bg-transparent hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
               My Projects
             </Link>
 
             <Link
               href="/contact-me"
-              className=" group relative rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+              className=" group relative rounded-[20px] border border-white bg-transparent hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              
-              Contact me
+              contact-me
             </Link>
           </div>
+
           <div className=" absolute bottom-0 right-0 z-[10]">
             <Image
             src="/horse.png"
@@ -84,6 +83,7 @@ export default function Home() {
              height={480}
              />
           </div>
+
           <div className=" absolute bottom-0 z-[5] w-full h-full">
           <Image src="/trees.webp" alt="trees" width={2000} height={2000}
           className="w-full h-full"/>
@@ -96,6 +96,7 @@ export default function Home() {
             className="absolute left-0 top-10 z-[10]"
              />
           </div>
+
     </main>
   );
 }

6481989677c10aaf23525d4ef11e1234e9c5b362
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2024-01-17 00:41:06 +0530

    resources updated

--- app/About-me/page.tsx
+++ app/About-me/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import AppCard from "../components/AppCard";
-import { AppProjects } from "../constants/index";
+
 import Image from "next/image";
 import React from "react";
 const Page = () => {
